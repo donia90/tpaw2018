e1ab0d2985b09e7e6f8962fc2300448b7da90311
Donia <32840303+donia90@users.noreply.github.com>
2017-11-15 21:48:24 +0100

    Add files via upload

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
              console.log("click");  
     
              if($("#nom").val().length<5 || $("#prenom").val().length<5 ||
-                $("#adresse").val().length<5 || $("#mail").val().length<5 || $("#date".val()==""))
+                $("#addresse").val().length<5 || $("#mail").val().length<5 || $("#datepicker".val()==""))
              {
     
                 $(".modal-title").text("Message Modal");
@@ -18,10 +18,10 @@ $( document ).ready(function() {
     
                 $(".modal-title").text("Bienvenu   " + $("#nom").val()+" "+$("#prenom"));
     
-                $(".modal-body").html(' Votre date de naissance est : ' + $("#date").val() + 
+                $(".modal-body").html(' Votre date de naissance est : ' + $("#datepicker").val() + 
                 '</br>'  + 'Et vous habitez : ' + '</br>'  + 
                 '<img src="https://maps.googleapis.com/maps/api/staticmap?center=Noisy%20Le%Grand&markers=Noisy%20Le%Grand&size=800x400&zoom=12&key=AIzaSyBAIbFV9W1NopwHu8RFS06YugjVXqn6ewc"  height="260" width="420"/>' 
-                 + '</br>' + '<a href="http://maps.google.com/maps?q=Noisy-Le-Grand">' + $("#adresse").val() +  '</a>' );
+                 + '</br>' + '<a href="http://maps.google.com/maps?q=Noisy-Le-Grand">' + $("#addresse").val() +  '</a>' );
     
              }
     
