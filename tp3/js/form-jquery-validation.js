f363f8a70ff75ca4eb0911df7ae3d78491258b54
Donia <32840303+donia90@users.noreply.github.com>
2017-11-15 22:33:11 +0100

    Add files via upload

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,31 +1,30 @@
 $( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+     $("#submit").on("click",function(){ 
+  //console event.preventDefault(); 
+   console.log( "click" ); 
+ 
+     // Y mettre le code jQuery pour valider tous les champs du formulaire
+     if ($("#nom").val().length>4 && $("#prenom").val().length>4 && $("#mail").val().length>4 && $("#addresse").val().length>4 ){
+ 
+      $('#myModal').modal("show");
+      $(".modal-title").text("Bienvenue "+ $("#Nom").val());
+      $(".modal-body").html('Vous etes né(e) le '+ $("#date").val());
+      $(".modal-body").html('<a href="http://maps.google.com/maps?q='+ $("#addresse").val()+'"> <img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#addresse").val()+ ',&zoom=13&size=400x400"/></a> <br></br>'
+          +'et vous habitez <a href="http://maps.google.com/maps?q='+ $("#addresse").val()+'">'+ $("#addresse").val()+'</a>');
+     
     
-         console.log( "DOM ready!" );
-    
-         $("#submit").on("click",function(event){
-             event.preventDefault();
-             console.log("click");  
-    
-             if($("#nom").val().length<5 || $("#prenom").val().length<5 ||
-                $("#addresse").val().length<5 || $("#mail").val().length<5 || $("#datepicker".val()==""))
-             {
-    
-                $(".modal-title").text("Message Modal");
-                $(".modal-body").html('Veuillez remplir tous les champs SVP');
-    
-             }
-             else{
-    
-                $(".modal-title").text("Bienvenu   " + $("#nom").val()+" "+$("#prenom"));
-    
-                $(".modal-body").html(' Votre date de naissance est : ' + $("#datepicker").val() + 
-                '</br>'  + 'Et vous habitez : ' + '</br>'  + 
-                '<img src="https://maps.googleapis.com/maps/api/staticmap?center=Noisy%20Le%Grand&markers=Noisy%20Le%Grand&size=800x400&zoom=12&key=AIzaSyBAIbFV9W1NopwHu8RFS06YugjVXqn6ewc"  height="260" width="420"/>' 
-                 + '</br>' + '<a href="http://maps.google.com/maps?q=Noisy-Le-Grand">' + $("#addresse").val() +  '</a>' );
-    
-             }
-    
-             $("#myModal").modal("show");
-         });
-    
-     });
+      }
+      else
+      {
+       $('#myModal').modal("show");
+      $(".modal-title").text("Message Modal");
+      $(".modal-body").html('Veuillez remplir tous les champs SVP !');
+      
+      }
+  });
+ 
+ 
+ });
